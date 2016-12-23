03602461aed1b8a01d13d4752fc010f071ef7e74
bwynne <bwynne@10.152.189.96>
2016-12-23 10:35:07 -0500

    Adding initial files.

---
+++ LSCSWSReplacement/src/main/java/gov/nyc/doitt/pressreleases/LscswsReplacementApplication.java
@@ -0,0 +1,12 @@
+package gov.nyc.doitt.pressreleases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LscswsReplacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LscswsReplacementApplication.class, args);
+	}
+}
---
+++ LSCSWSReplacement/src/test/java/gov/nyc/doitt/pressreleases/LscswsReplacementApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.nyc.doitt.pressreleases;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LscswsReplacementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
